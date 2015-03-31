9295575fdb77ecdda9c00e2c451fa6bd29087270
Qian Long <qlong@mit.edu>
2015-03-31 15:13:00 -0400

    Added data gen script

---
+++ scripts/gen_input_by_lines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import random
+
+def generate(LIMIT, num_cells, num_col):
+  for i in xrange(num_cells):
+    string = ""
+    for j in xrange(num_col-1):
+      string = string + str(int(random.random()*LIMIT)) + ","
+
+    string = string + str(int(random.random()*LIMIT))
+    print string
+
+def uniform(start, end, num_cells, num_cols):
+ for i in xrange(num_cells):
+  string = ""
+  for j in xrange(num_cols-1):
+    string = string + str(random.randint(start, end)) + ","
+
+  string = string + str(random.randint(start, end))
+  print string
+
+def skewed(start, end, num_lines, num_cols):
+  pass
+
+if __name__ == "__main__":
+  uniform(0, 1000000, 100000, 4)
