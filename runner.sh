fa50b05e46321a0775ff0c6a1fdddba7c56ddc91
Qian Long <qlong@mit.edu>
2015-05-02 00:44:20 -0400

    Modified machinefile

--- runner.sh
+++ runner.sh
@@ -1,21 +1,24 @@
 #! /bin/bash
-NUM_NODES=3
-DATASET1=ais_2009_01_allzones
-#DATASET1=test_D
+declare -a NODES=("istc2" "istc3" "istc4" "istc5" "istc6" "istc7" "istc12" "istc13")
+NUM_NODES=$1
+#DATASET1=ais_2009_01_allzones
+DATASET1=test_D
 FILENAME1=$DATASET1.csv
-DATASET2=ais_2009_09_allzones
-#DATASET2=test_E
+#DATASET2=ais_2009_09_allzones
+DATASET2=test_E
 FILENAME2=$DATASET2.csv
 TEMP_NAME=$( date +"%m-%dT%H:%M")
-TRIAL=1
+TRIAL=$2
 RUN_NAME=n$NUM_NODES\_t$TRIAL\_$DATASET1\_$DATASET2\_$TEMP_NAME
 DATA_FOLDER=Result/$RUN_NAME
 mkdir -p $DATA_FOLDER
 
 #clear the machines for running
-for i in `seq 2 $NUM_NODES`;
+for i in `seq 0 $(($NUM_NODES-2))`;
 do
-  ssh -i ~/.ssh/qlong istc$i -t "cd ~/TileDB-multinode; make multi-istc; ./setup_env.sh"
+  NODE="${NODES[$i]}"
+  echo setting up node $NODE
+  ssh -i ~/.ssh/qlong $NODE -t "cd ~/TileDB-multinode; make multi-istc; ./setup_prod.sh; /usr/local/bin/drop_caches"
 done;
 
 #1 = datatsize, 2 = number of nodes including master, 3 = trial number
@@ -33,12 +36,16 @@ if [ "$?" -eq "0" ]
 then
   #get data from master
   cp /data/qlong/workspaces/workspace-0/logfile $DATA_FOLDER/master_logfile.txt
-  for i in `seq 2 $NUM_NODES`;
+  for i in `seq 0 $(($NUM_NODES-2))`;
   do
-    WS_PATH=/data/qlong/workspaces/workspace-$(($i-1))
-    ssh -i ~/.ssh/qlong istc$i -t "du -a $WS_PATH > $WS_PATH/sizes.txt"
-    scp istc$i:$WS_PATH/logfile $DATA_FOLDER/istc$i\_log.txt
-    scp istc$i:$WS_PATH/sizes.txt $DATA_FOLDER/istc$i\_sizes.txt
+    WS_PATH=/data/qlong/workspaces/workspace-$(($i+1))
+    NODE="${NODES[$i]}"
+
+    echo grabbing files from $WS_PATH on node $NODE
+
+    ssh -i ~/.ssh/qlong $NODE -t "du -ab $WS_PATH > $WS_PATH/sizes.txt"
+    scp -i ~/.ssh/qlong $NODE:$WS_PATH/logfile $DATA_FOLDER/$NODE\_log.txt
+    scp -i ~/.ssh/qlong $NODE:$WS_PATH/sizes.txt $DATA_FOLDER/$NODE\_sizes.txt
   done;
 else
   echo "Test script failed"
